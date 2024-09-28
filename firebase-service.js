21c0daed11cd258baad04404135fbb76734676d6
Indro Lie <indro@vouch.sg>
2024-09-28 19:08:29 +0700

    [feat] initialise firebase for notification handling

---
+++ firebase-service.js
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+admin.initializeApp({
+	credential: admin.credential.cert({
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+		privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // prevent newline issues
+	})
+});
+
+const sendNotification = (deviceId, messageText) => {
+  	const message = {
+		notification: {
+			title: 'Incoming message',
+			body: messageText,
+		},
+		token: deviceId,
+	};
+
+	return admin.messaging().send(message);
+};
+
+module.exports = { sendNotification };
